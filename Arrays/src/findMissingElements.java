ad1855ccb8873630c237e48670b9ec4d7528de23
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2025-05-25 14:45:08 +0500

    perf: optimize finding missing number from a sorted array from time complexity O(n^2)to O(n).

--- Arrays/src/findMissingElements.java
+++ Arrays/src/findMissingElements.java
@@ -2,26 +2,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class findMissingElements {
-    public int findMissing(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i <= n + 1; i++) {
-            boolean found = false;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] == i) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                return i;
-            }
+//    public int findMissing(int[] arr) {
+//        int n = arr.length;
+//        for (int i = 0; i <= n + 1; i++) {
+//            boolean found = false;
+//            for (int j = 0; j < arr.length; j++) {
+//                if (arr[j] == i) {
+//                    found = true;
+//                    break;
+//                }
+//            }
+//            if (!found) {
+//                return i;
+//            }
+//        }
+//        return -1; // Return -1 if no missing element is found
+//    }
+
+    //=================OPTIMAL SOLUTION=======================================
+
+    public int findMissing(int[]arr){
+
+        int n = arr.length+1; // The length of the array plus one (the missing number)
+        int sum = (n*(n+1))/2;
+        int k = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            k = k+arr[i];
         }
-        return -1; // Return -1 if no missing element is found
+        
+//        int missing = -1;
+//        if (k != sum){
+//            return missing = sum-k;
+//        }
+//        return missing;
+
+        return sum-k;
     }
 
     public static void main(String[] args) {
         findMissingElements obj = new findMissingElements();
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = {1, 2, 3, 5, 6};
         int missing = obj.findMissing(arr);
         System.out.println(missing);
     }
